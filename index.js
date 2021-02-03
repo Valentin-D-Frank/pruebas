3cf8651037ba274d551133fb46a8a9ba28800df1
David Frank Valentin Ricaldi <david.valentin@unmsm.edu.pe>
2021-02-03 02:25:46 -0500

    Frank f

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require('./app')
-const GlobalEnv = require('./GlobalEnv')
+const app = require('./Desarrollo/SOE/Fuentes/app')
+const GlobalEnv = require('./Desarrollo/SOE/Fuentes/GlobalEnv')
 
 app.listen(GlobalEnv.port, () => {
     console.log(`Servidor web escuchando en ${GlobalEnv.host}:${GlobalEnv.port}`)
