81f0135da9ae1bd1bded75cf10e6dda1df27b5f2
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2021-02-03 03:59:02 -0500

    oinoinv

--- index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./Desarrollo/SOE/Fuentes/app')
-const GlobalEnv = require('./Desarrollo/SOE/Fuentes/GlobalEnv')
-
-app.listen(GlobalEnv.port, () => {
-    console.log(`Servidor web escuchando en ${GlobalEnv.host}:${GlobalEnv.port}`)
-})
